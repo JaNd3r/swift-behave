eab1360b1a04a9d31bbd25608aa819bb1d406762
Christian Klaproth <christian.klaproth@gebit.de>
2016-05-12 15:18:57 +0200

    Refactored as extension...
    SwiftBehaveTests is now an extension of XCTestCase, example app has
    been extended by a sample implementation and test story.

---
+++ TestApp/TestAppUITests/Test.story
@@ -0,0 +1,26 @@
+Narrative: An example story test file.
+
+Scenario: Check initial list is empty
+Given the main view is shown
+Then I expect to see 0 items
+
+Scenario: Create one item
+Given the main view is shown
+When I tap the add button
+Then I expect to see 1 items
+
+Scenario: Create multiple items
+Given the main view is shown
+When I tap the add button
+And I tap the add button
+Then I expect to see 3 items
+
+Scenario: Navigate to details
+Given the main view is shown
+When I tap on item at position 1
+Then I expect to see the details
+
+Scenario: Delete items
+Given the main view is shown
+When I delete the item at position 2
+Then I expect to see 2 items
--- TestApp/TestAppUITests/TestAppUITests.swift
+++ TestApp/TestAppUITests/TestAppUITests.swift
@@ -8,14 +8,78 @@
 
 import XCTest
 
-class TestAppUITests: SwiftBehaveTests {
-            
+class TestAppUITests: XCTestCase {
+    
+    override func mappingFromPlist() -> Dictionary<String, String> {
+        
+        var mapping: Dictionary<String, String> = Dictionary()
+        
+        mapping["the main view is shown"] = "givenMainView"
+        mapping["I tap the add button"] = "whenTapAddButton"
+        mapping["I expect to see $count items"] = "thenCheckItemCount"
+        mapping["I tap on item at position $position"] = "whenTapItemAtPosition"
+        mapping["I expect to see the details"] = "thenDetailsVisible"
+        mapping["I delete the item at position $position"] = "whenDeleteItemAtPosition"
+        
+        return mapping
+    }
+    
     func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        self.testStories()
     }
     
     // MARK: -
     // MARK: BDD functions
 
+    func givenMainView(app: XCUIApplication) {
+        
+        let detailNavigationBar = app.navigationBars["Detail"]
+        if (detailNavigationBar.exists) {
+            detailNavigationBar.buttons.elementBoundByIndex(0).tap()
+        }
+        
+        let masterNavigationBar = app.navigationBars["Master"]
+        
+        XCTAssert(masterNavigationBar.exists, "main view could not be opened")
+    }
+    
+    func whenTapAddButton(app: XCUIApplication) {
+        
+        app.buttons["Add"].tap()
+    }
+    
+    func thenCheckItemCount(app: XCUIApplication, count: String) {
+        
+        let expectedCount = UInt(count)!
+        let actualCount = app.tables.elementBoundByIndex(0).cells.count
+        
+        XCTAssert(expectedCount == actualCount, "actual item count (\(actualCount)) does not match expected item count (\(expectedCount))")
+    }
+    
+    func whenTapItemAtPosition(app: XCUIApplication, position: String) {
+        
+        let index = UInt(position)! - 1
+        app.tables.elementBoundByIndex(0).cells.elementBoundByIndex(index).tap()
+    }
+    
+    func thenDetailsVisible(app: XCUIApplication) {
+        
+        let detailNavigationBar = app.navigationBars["Detail"]
+        
+        XCTAssert(detailNavigationBar.exists, "details not shown")
+    }
+    
+    func whenDeleteItemAtPosition(app: XCUIApplication, position: String) {
+        
+        app.buttons["Edit"].tap()
+        
+        let index = UInt(position)! - 1
+        // delete
+        app.tables.elementBoundByIndex(0).cells.elementBoundByIndex(index).buttons.elementBoundByIndex(0).tap()
+        // confirm
+        app.tables.elementBoundByIndex(0).cells.elementBoundByIndex(index).buttons["Delete"].tap()
+        
+        app.buttons["Done"].tap()
+    }
+    
 }
